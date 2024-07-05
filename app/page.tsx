03dd3e225eec82eaf9e0e7e1c8d20d742a7126cc
jesusalejodev <jesusalejowk@gmail.com>
2024-07-05 13:16:43 +0200

    minor fix.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 import Hero from "@/components/Hero";
 import Grid from "@/components/Grid";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
-import Image from "next/image";
 import { FaHome } from "react-icons/fa";
-import RecentProjects from "@/components/RecentProjects";
 
 export default function Home() {
   return (
