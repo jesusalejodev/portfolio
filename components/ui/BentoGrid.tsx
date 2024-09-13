737eebf67103e6ef3a504574125fd06c902439f7
jesusalejodev <jesusalejowk@gmail.com>
2024-09-13 12:37:18 +0200

    mail adress fix

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -61,7 +61,7 @@ export const BentoGridItem = ({
   };
 
   const handleCopy = () => {
-    const text = "hsu@jsmastery.pro";
+    const text = "jesusalejowk@gmail.com";
     navigator.clipboard.writeText(text);
     setCopied(true);
   };
